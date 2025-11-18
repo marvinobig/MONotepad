a68fcdc1bf0e456392442fddf00b44bc8b83ef2e
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-11-18 11:15:18 +0100

    add main controls for app functionality (menu strip, status bar and rich text box)

--- MONotepad/Form1.Designer.cs
+++ MONotepad/Form1.Designer.cs
@@ -28,18 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStrip1 = new MenuStrip();
+            statusStrip1 = new StatusStrip();
+            richTextBox1 = new RichTextBox();
             SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(384, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Location = new Point(0, 339);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(384, 22);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(0, 27);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(384, 309);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(384, 361);
+            Controls.Add(richTextBox1);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "MONotepad";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private StatusStrip statusStrip1;
+        private RichTextBox richTextBox1;
     }
 }
